0e34a339203bef10fefc2ef1dbcec31ead808e20
Alex <alex@likening.co.uk>
2022-12-22 13:49:34 +0000

    update

--- lib/screens/chatPage.dart
+++ lib/screens/chatPage.dart
@@ -1,3 +1,4 @@
+import 'package:basic_messaging_app/models/chatUsersModel.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -6,6 +7,17 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<ChatUsers> chatUsers = [
+    ChatUsers(name: "Jane Russel", messageText: "Awesome Setup", imageURL: "images/userImage1.jpg", time: "Now"),
+    ChatUsers(name: "Glady's Murphy", messageText: "That's Great", imageURL: "images/userImage2.jpg", time: "Yesterday"),
+    ChatUsers(name: "Jorge Henry", messageText: "Where are you?", imageURL: "images/userImage3.jpg", time: "2 Days Ago"),
+    ChatUsers(name: "Philip Fox", messageText: "I'm Busy", imageURL: "images/userImage4.jpg", time: "Last Week"),
+    ChatUsers(name: "Debra Hawkins", messageText: "Thank you", imageURL: "images/userImage5.jpg", time: "2 Weeks Ago"),
+    ChatUsers(name: "Jacob Pena", messageText: "Will update you", imageURL: "images/userImage6.jpg", time: "2 Weeks Ago"),
+    ChatUsers(name: "Andrey Jones", messageText: "Hello", imageURL: "images/userImage7.jpg", time: "Last Month"),
+    ChatUsers(name: "John Wick", messageText: "How are you?", imageURL: "images/userImage8.jpg", time: "Last Year"),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,6 +61,25 @@ class _ChatPageState extends State<ChatPage> {
                       ),
                     )
                   ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Colors.grey.shade600),
+                  prefixIcon: Icon(Icons.search, color: Colors.grey.shade600, size: 20,),
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  contentPadding: EdgeInsets.all(8),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade100
+                    )
+                  )
                 ),
               ),
             ),
